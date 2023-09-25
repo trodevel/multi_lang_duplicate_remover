dfa48b7abcf0283a023704402b9e4dbb7de54e09
trodevel <trodevel@gmail.com>
2023-09-25 02:04:48 +0200

    intermediate results

--- prepare.sh
+++ prepare.sh
@@ -12,9 +12,15 @@ uniq $FO.srt.csv > $FO.srt.uniq.csv
 
 # enumerate
 nl -s";" -nln -w1 $FO.srt.uniq.csv > $FL.srt.uniq.num.csv
+
+RU="а-я"
+DE="äöüß"
+EN="a-z"
 # ru
-grep -i "[а-я]+"  $FL.srt.uniq.num.csv > $FL.srt.uniq.num.ru.csv
+grep -i "[$RU]\+"  $FL.srt.uniq.num.csv > $FL.srt.uniq.num.ru.csv
 # de
-grep -i "[äöüß]+" $FL.srt.uniq.num.csv > $FL.srt.uniq.num.de.csv
+grep -i "[$DE]\+" $FL.srt.uniq.num.csv > $FL.srt.uniq.num.de.csv
 # en
-grep -v -i "[а-яäöüß]+" $FL.srt.uniq.num.csv > $FL.srt.uniq.num.en.csv
+grep -i "[$EN]\+" $FL.srt.uniq.num.csv | grep -v -i "[${RU}${DE}]\+" > $FL.srt.uniq.num.en.csv
+# other
+grep -v -i "[${RU}${DE}${EN}]\+"  $FL.srt.uniq.num.csv> $FL.srt.uniq.num.other.csv
