a1e6b544c75ff059716007359d1b91142ab99e00
trodevel <trodevel@gmail.com>
2023-09-29 02:18:36 +0200

    renaming

--- prepare.sh
+++ prepare.sh
@@ -7,20 +7,20 @@ FL=$1
 
 FO="${FL%%.*}"
 
-sort $FL > $FO.srt.csv
-uniq $FO.srt.csv > $FO.srt.uniq.csv
+sort $FL > $FO.1.csv
+uniq $FO.1.csv > $FO.2.csv
 
 # enumerate
-nl -s";" -nln -w1 $FO.srt.uniq.csv > $FO.srt.uniq.num.csv
+nl -s";" -nln -w1 $FO.2.csv > $FO.3.csv
 
 RU="а-я"
 DE="äöüß"
 EN="a-z"
 # ru
-grep -i "[$RU]\+"  $FO.srt.uniq.num.csv > $FO.srt.uniq.num.ru.csv
+grep -i "[$RU]\+"  $FO.3.csv > $FO.4.ru.csv
 # de
-grep -i "[$DE]\+" $FO.srt.uniq.num.csv > $FO.srt.uniq.num.de.csv
+grep -i "[$DE]\+" $FO.3.csv > $FO.4.de.csv
 # en
-grep -i "[$EN]\+" $FO.srt.uniq.num.csv | grep -v -i "[${RU}${DE}]\+" > $FO.srt.uniq.num.en.csv
+grep -i "[$EN]\+" $FO.3.csv | grep -v -i "[${RU}${DE}]\+" > $FO.4.en.csv
 # other
-grep -v -i "[${RU}${DE}${EN}]\+"  $FO.srt.uniq.num.csv> $FO.srt.uniq.num.other.csv
+grep -v -i "[${RU}${DE}${EN}]\+"  $FO.3.csv> $FO.4.other.csv
