e4c0b5223c6ba437bc7fd165eb793f1dc3c6f2c8
trodevel <trodevel@gmail.com>
2023-09-29 02:20:57 +0200

    renaming

--- prepare_2.sh
+++ prepare_2.sh
@@ -10,11 +10,11 @@ OUTP=$2
 #FO="${INP%%.*}"
 FO="$INP"  # work-around
 
-./convert_xlsx_to_csv.sh $FO.srt.uniq.num.en.ru.xlsx
-./convert_xlsx_to_csv.sh $FO.srt.uniq.num.ru.en.xlsx
+./convert_xlsx_to_csv.sh $FO.4.en.ru.xlsx
+./convert_xlsx_to_csv.sh $FO.4.ru.en.xlsx
 
 # ru
-cat $FO.srt.uniq.num.ru.csv $FO.srt.uniq.num.en.ru.csv | sort -n > $OUTP.ru.csv
+cat $FO.4.ru.csv $FO.4.en.ru.csv | sort -n > $OUTP.ru.csv
 
 # en
-cat $FO.srt.uniq.num.en.csv $FO.srt.uniq.num.ru.en.csv | sort -n > $OUTP.en.csv
+cat $FO.4.en.csv $FO.4.ru.en.csv | sort -n > $OUTP.en.csv
